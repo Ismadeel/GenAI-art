f17aa1b0903ef740137ac559a3df207d31da90a1
Adeel <adeel.yusuf@gmail.com>
2023-08-04 12:11:29 +0500

    AI TxT2IMG Intership Project

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import replicate
 
 os.environ["REPLICATE_API_TOKEN"]='<YOUR API KEY>'
 
+
 output = replicate.run(
     "stability-ai/sdxl:2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2",
     input={"prompt": "an astronaut riding a rainbow unicorn"},
 )
 # Zip file containing input images, hosted somewhere on the internet
-zip_url = "https://drive.google.com/uc?export=download&id=1BPdPj5s5R9ejpXaWHXHVgxsTQsU4A-ZQ"
+# zip_url = "https://drive.google.com/uc?export=download&id=1BPdPj5s5R9ejpXaWHXHVgxsTQsU4A-ZQ"
 
-# Train the model
-lora_url = replicate.run(
-    "replicate/lora-training:b2a308762e36ac48d16bfadc03a65493fe6e799f429f7941639a6acec5b276cc",
-    input={"instance_data": zip_url, "task": "style"}
-)
+# # Train the model
+# lora_url = replicate.run(
+#     "replicate/lora-training:b2a308762e36ac48d16bfadc03a65493fe6e799f429f7941639a6acec5b276cc",
+#     input={"instance_data": zip_url, "task": "style"}
+# )
 
 
 lora_url = "https://replicate.delivery/pbxt/fdtiMuQFF1yQAS22N4JWR3epRbW7ea5shRBsSmFqJBceBTZFB/tmpmzwgpo3puc.safetensors"
@@ -27,4 +28,4 @@ output_url = replicate.run(
     },
 )
 
-# print output_url
+print(output_url)
